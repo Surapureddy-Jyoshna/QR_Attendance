493a85a57b996558a0f6d6e921b1c66b150ba312
Surapureddy-Jyoshna <surapureddyjyoshna18@gmail.com>
2026-04-11 20:31:56 +0530

    First commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,13 +9,12 @@ const csv = require("csv-parser");
 
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb://surapureddyjyoshna18_db_user:jyoshna%401418@ac-8ljpoyh-shard-00-00.r74rbfc.mongodb.net:27017,ac-8ljpoyh-shard-00-01.r74rbfc.mongodb.net:27017,ac-8ljpoyh-shard-00-02.r74rbfc.mongodb.net:27017/College?ssl=true&replicaSet=atlas-5jt819-shard-0&authSource=admin&retryWrites=true&w=majority"
-)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log("Mongo Error ❌", err));
 
@@ -351,6 +351,8 @@ app.post("/student/mark-attendance", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
