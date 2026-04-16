467b78975a2a933e185a18331ee30707a53247de
Surapureddy-Jyoshna <surapureddyjyoshna18@gmail.com>
2026-04-16 21:33:37 +0530

    modified

--- backend/server.js
+++ backend/server.js
@@ -464,7 +464,7 @@ if(
   console.log("Accuracy:", req.body.accuracy);
 
   // ✅ Strict 100m check
-  if(distance > 100){
+  if(distance > 400){
     return res.json({
       success: false,
       message: `You are ${Math.round(distance)} meters away`
